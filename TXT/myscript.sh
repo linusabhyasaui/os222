d96d03fe561a94a88d384bf3203377f2872cc2d2
linus <linus.abhyasa@ui.ac.id>
2022-10-03 21:57:09 +0800

    W04 redone

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -15,7 +15,7 @@
 REC2="142593FFCE349594"
 # ####################### ####### #### ####
 # REC1: public key
-REC1="63FB12B215403B20"
+REC1="142593FFCE349594"
 # WEEKURL="http://localhost:4000/WEEK/WEEK.txt"
 WEEKURL="https://os.vlsm.org/WEEK/WEEK.txt"
 FILES="my*.asc my*.txt my*.sh"
